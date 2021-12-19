8b4e06e53ea6935980291ee945287440ff64e6b1
mfumanelli <fumanellimartina@gmail.com>
2021-12-19 14:36:58 +0100

    add code for chinese character scraping

--- character_scraping.py
+++ character_scraping.py
@@ -1,15 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import yaml
 
-# Create an URL object
-url = 'https://blog.lingodeer.com/chinese-hsk-1-vocabulary-list/'
-# Create object page
-page = requests.get(url)
+with open(r'config.yaml') as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
 
-# parser-lxml = Change html to Python friendly format
-# Obtain page's information
-soup = BeautifulSoup(page.text, 'lxml')
+# create url object
+URL = config['character_scraping']['url']
 
-table1 = soup.find('table')
-print(table1)
+
+def character_scraping(url=URL):
+    # create object page
+    page = requests.get(URL)
+
+    # obtain page's information
+    soup = BeautifulSoup(page.text, 'lxml')
+
+    table = soup.find('table')
+
+    # create a column list
+    headers = []
+    for i in table.find_all('th'):
+        title = i.text
+        headers.append(title)
+
+    # create a dataframe
+    data = pd.DataFrame(columns=headers)
+
+    for j in table.find_all('tr')[1:]:
+        row_data = j.find_all('td')
+        row = [i.text for i in row_data]
+        length = len(data)
+        data.loc[length] = row
+
+    return data
