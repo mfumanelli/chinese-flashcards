c2f51619a9f6bc7cdab5ab2db0ffc6b4b0c05cd6
mfumanelli <fumanellimartina@gmail.com>
2021-12-20 15:07:21 +0100

    add translation function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from flask import Flask, render_template, request
+from character_scraping import character_scraping
 
 app = Flask(__name__)
 
@@ -12,7 +13,24 @@ def main():
 
 @app.route('/flashcards', methods=['GET', 'POST'])
 def flashcards():
-    return render_template("flashcards.html")
+    df = character_scraping()
+    df_chinese = df['Chinese']
+    df_pinyin = df['Pinyin']
+    df_Role = df['Role']
+    df_english = df['English']
+    return render_template("flashcards.html", flashcards_chinese=df_chinese[0], pinyin=df_pinyin[0],
+                           flashcards_english=df_english[0])
+
+
+@app.route('/flashcards-translation', methods=['GET', 'POST'])
+def translation():
+    df = character_scraping()
+    df_chinese = df['Chinese']
+    df_pinyin = df['Pinyin']
+    df_Role = df['Role']
+    df_english = df['English']
+    return render_template("translation.html", flashcards_chinese=df_chinese[0], pinyin=df_pinyin[0],
+                           flashcards_english=df_english[0])
 
 
 if __name__ == "__main__":
